a17ac9f09490440916c6935be4b2744d777687b5
HBE17 <hicham-boutaghou@hotmail.co.uk>
2022-11-03 15:46:06 +0000

    Created kata and the tests for the challenge

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 /* 
 👉 Write your kata here!
 */
+//Ive lost my pyjamas
+//array of strings of anagrams of different types of clothes and you have to return "pyjamas"
+//The challenge is to return the position of the array that contains the string pyjamas
+//Asnwer must be a number
 
 //👉 Write the function your CodeWarriors will start with below here:
+export function findPyjamas(arr) {
+  //   let wordReg = null;
+  for (let i = 0; i < arr.length; i++) {
+    // console.log((arr[i].match(/[pyjamas]/g) || []).length);
+    if ((arr[i].match(/[pyjamas]/g) || []).length === arr[i].length) {
+      return i;
+    }
+  }
+
+  //CODE GOES HERE
+}
+
+// console.log(findPyjamas(["cosks", "ath", "ckjtae", "saamypj", "olgevs"]));
+// ;
--- main.test.js
+++ main.test.js
@@ -1,1 +1,24 @@
+import { findPyjamas } from "./main.js";
+import { test, expect } from "@jest/globals";
 //👉 Write your tests below here:
+
+//test1.
+test("Check if position returned is correct", () => {
+  const actual = findPyjamas(["cosks", "ath", "ckjtae", "saamypj", "olgevs"]);
+  const expected = 3;
+  expect(actual).toBe(expected);
+});
+
+//test2.
+test("check if a different position is correct", () => {
+  const actual = findPyjamas(["cosks", "ath", "ckjtae", "olgevs", "saamypj"]);
+  const expected = 4;
+  expect(actual).toBe(expected);
+});
+
+//test3.
+test("check a number is returned", () => {
+  const actual = findPyjamas(["cosks", "ath", "ckjtae", "olgevs", "saamypj"]);
+  const expected = "number";
+  expect(typeof actual).toBe(expected);
+});
